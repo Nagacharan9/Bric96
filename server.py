24e6c276198ae22f2396d0277b314f2841d0849e
Ronda Naga Charan <charannaga48@gmail.com>
2026-04-03 11:03:06 +0530

    Enable unsafe Werkzeug for socketio server

--- server.py
+++ server.py
@@ -33,4 +33,4 @@ def ping():
     return "pong", 200
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=False)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=False, allow_unsafe_werkzeug=True)
